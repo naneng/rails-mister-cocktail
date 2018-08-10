ee28609c81a2a34b6dd63ecd5f0f126a998b995c
Katrina Ariola <kat3ariola@gmail.com>
2018-08-10 19:26:19 +0900

    updated cocktailshelper and cocktail index for error

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,9 @@
 module CocktailsHelper
+  def cocktail_photo_or_default(cocktail)
+    if cocktail.photo.present?
+      cl_image_path cocktail.photo, height: 300, width: 400, crop: :fill
+    else
+      image_path 'classicmargarita.jpg'
+    end
+  end
 end
