0d5838e7ef62d63b6c2991a8f75b891f56ac28d4
Katrina Ariola <kat3ariola@gmail.com>
2018-08-10 15:07:08 +0900

    updated forms

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,24 +5,29 @@ class DosesController < ApplicationController
   # end
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.save!
+    @dose.save
     if @dose.save
-      redirect_to cocktail_path(@dose.cocktail)
+      # redirect_to cocktail_path(@dose.cocktail)
+      redirect_to cocktail_path(params[:cocktail_id])
     else
       render :new
     end
   end
 
   def destroy
+    # @dose = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
     @dose.destroy
 
-    redirect_to cocktails_path(@cocktail)
+    redirect_to cocktail_path(cocktail)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [ :index, :show, :new, :create ] do
     resources :ingredients, only: [ :new, :create ]
-    resources :doses, only: [ :new, :create, :destroy ]
+    resources :doses, only: [ :new, :create ]
   end
+  resources :doses, only: [:destroy ]
 
-  # resources :restaurants do
-  #   resources :reviews, only: [ :index, :new, :create ]
-  # end
-  # resources :reviews, only: [ :show, :edit, :update, :destroy ]
-
-
-  # get "restaurants/:restaurant_id/reviews",     to: "reviews#index"
-  # get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
-  # post "restaurants/:restaurant_id/reviews",    to: "reviews#create"
-  # get "reviews/:id",                            to: "reviews#show"
-  # get "reviews/:id/edit",                       to: "reviews#edit"
-  # patch "reviews/:id",                          to: "reviews#update"
-  # delete "reviews/:id",                         to: "reviews#destroy"
 end
+
