7467863f30f077428319232b5a30ebc0868fa007
Katrina Ariola <kat3ariola@gmail.com>
2018-08-12 01:17:23 +0900

    updated destroy on cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -22,14 +22,13 @@ class CocktailsController < ApplicationController
     else
       render :new
     end
+  end
 
   def destroy
+    @cocktail = Cocktail.find(params[:id])
     @cocktail.destroy
 
     redirect_to cocktails_path
-  end
-
-
   end
 
   private
